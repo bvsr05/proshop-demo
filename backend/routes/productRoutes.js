b4d2b5b733acaeb8ced7f05ac9003d8e5f161c64
bvsr05 <40656754+bvsr05@users.noreply.github.com>
2024-08-09 17:05:03 +0530

    Add files via upload

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,33 +5,37 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
-  //createProduct,
-  //updateProduct,
-  //deleteProduct,
-  //createProductReview,
-  //getTopProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js';
-/*
-//import { protect, admin } from '../middleware/authMiddleware.js';
+
+import { protect, admin } from '../middleware/authMiddleware.js';
 //import checkObjectId from '../middleware/checkObjectId.js';
-*/
-
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
 
 
-
-
-
-/*
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 router.get('/top', getTopProducts);
+
+router.route('/:id')
+.get(getProductById)
+.put(protect, admin, updateProduct)
+.delete(protect, admin, deleteProduct);
+/*
 router
   .route('/:id')
   .get(checkObjectId, getProductById)
   .put(protect, admin, checkObjectId, updateProduct)
-  .delete(protect, admin, checkObjectId, deleteProduct);
-  */
+  .delete(protect, admin, checkObjectId, deleteProduct);  
+*/
+
+router.route('/:id/reviews').post(protect, createProductReview);
+//router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
+
+
+
+
 
 export default router;
